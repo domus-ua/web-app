0d1f9a84f7ac4764b89b4b3c5915ad46bc234a04
Tiago Mendes <tiagocmendes@ua.pt>
2020-05-18 22:14:46 +0100

    Sort all houses

--- src/layouts/AllHouses/index.jsx
+++ src/layouts/AllHouses/index.jsx
@@ -6,6 +6,10 @@ import HouseCard from "components/HouseCard";
 import IconButton from '@material-ui/core/IconButton';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 import uris from "variables/uris";
 
@@ -15,9 +19,13 @@ class AllHouses extends React.Component {
         super(props);
         this.allHouses = [];
         this.state = {
+            sortingAttribute: "rating",
+            descendent: true,
+            orderBy: "",
             houses: []
         }
         this.fetchAllHouses = this.fetchAllHouses.bind(this);
+        this.orderHouses = this.orderHouses.bind(this);
     }
 
     componentDidMount() {
@@ -25,7 +33,10 @@ class AllHouses extends React.Component {
     }
 
     fetchAllHouses() {
-        fetch(uris.restApi.houses, {
+
+        let parameters = "?desc=" + this.state.descendent + "&orderAttribute=" + this.state.sortingAttribute;
+
+        fetch(uris.restApi.houses + parameters, {
             method: "GET",
             headers: {
                 "Accept": "application/json",
@@ -49,6 +60,40 @@ class AllHouses extends React.Component {
 
     }
 
+    orderHouses(event) {
+
+        let orderBy = event.target.value;
+
+        let sortingAttribute = orderBy.includes("rating") ? "rating" : "price";
+        let descendent = orderBy.includes("Higher") ? true : false;
+
+        let parameters = "?desc=" + descendent + "&orderAttribute=" + sortingAttribute;
+
+        fetch(uris.restApi.houses + parameters, {
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            }
+        })
+            .then(response => {
+                if (!response.ok) throw new Error(response.status);
+                else return response.json();
+
+            })
+            .then(data => {
+                this.allHouses = data;
+                this.setState({
+                    sortingAttribute: sortingAttribute,
+                    descendent: descendent,
+                    orderBy: orderBy,
+                    houses: this.allHouses.slice(0, 3) // FIXME: change later to 4
+                })
+            })
+            .catch(error => {
+                console.log("Fetch error: " + error);
+            })
+    }
 
     render() {
         return (
@@ -59,8 +104,25 @@ class AllHouses extends React.Component {
                 <section className="alternate">
                     <div className="container">
                         <div className="row">
-                            <div className="col-sm-4">
+                            <div className="col-sm-9">
                                 <h3 style={{ color: "#3f51b5" }}>All houses</h3>
+                            </div>
+                            <div className="col-sm-3">
+                                <FormControl variant="outlined" style={{ width: "100%" }}>
+                                    <InputLabel id="orderBy">Order by</InputLabel>
+                                    <Select
+                                        labelId="orderBy"
+                                        id="orderBy-select-outlined"
+                                        value={this.state.orderBy}
+                                        onChange={this.orderHouses}
+                                        label="orderBy"
+                                    >
+                                        <MenuItem value={"Higher rating"}>Higher rating</MenuItem>
+                                        <MenuItem value={"Lower rating"}>Lower rating</MenuItem>
+                                        <MenuItem value={"Higher price"}>Higher price</MenuItem>
+                                        <MenuItem value={"Lower price"}>Lower price</MenuItem>
+                                    </Select>
+                                </FormControl>
                             </div>
                         </div>
                         <div className="row">
