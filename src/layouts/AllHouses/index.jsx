9e818e0a70dd1b2c7068a5b4f56c6cca3b5a7f31
Tiago Mendes <tiagocmendes@ua.pt>
2020-05-18 17:30:15 +0100

    Search house done

---
+++ src/layouts/AllHouses/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class AllHouses extends React.Component {
+
+
+    render() {
+        return (
+            <div id="all-houses">
+                All Houses
+            </div>
+        );
+    }
+
+}
+
+export default AllHouses;
--- src/routers/publicRouter.js
+++ src/routers/publicRouter.js
@@ -2,7 +2,9 @@ import React from "react";
 import { Route } from "react-router-dom";
 
 import Home from "layouts/Home";
+import AllHouses from "layouts/AllHouses";
 
 export default [
+    { path: '/houses', layout: <Route path='/houses' component={AllHouses} /> },
     { path: '/', layout: <Route path='/' component={Home} /> },
 ]
