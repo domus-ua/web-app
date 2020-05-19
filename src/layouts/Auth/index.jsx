c4b1bfecca4381162e2293c4c0003006227064e2
Tiago Mendes <tiagocmendes@ua.pt>
2020-05-19 14:55:19 +0100

    Finish locatario sign in

--- src/layouts/Auth/index.jsx
+++ src/layouts/Auth/index.jsx
@@ -1,10 +1,36 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 
 import domusLogo from "assets/img/domus-logo.png";
 
 class Auth extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loaded: false,
+            authUser: JSON.parse(localStorage.getItem('authUser'))
+        }
+
+        this.authenticating = this.authenticating.bind(this);
+    }
+
+    componentDidMount() {
+        this.authenticating();
+    }
+
+    authenticating() {
+        setTimeout(() => {
+            this.setState({ loaded: true })
+        }, 2000);
+    }
+
     render() {
+        if (this.state.loaded) {
+            if (this.state.authUser.role === "locatario")
+                return <Redirect to="/locatario" />
+        }
         return (
             <section className="auth-banner">
                 <div className="gradient">
--- src/routers/protectedRouter.js
+++ src/routers/protectedRouter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 import Auth from "layouts/Auth";
+import HomeLocatario from "layouts/Locatario/Home";
 
 function PrivateRoute(props) {
     const path = props.path;
@@ -22,4 +23,5 @@ function PrivateRoute(props) {
 
 export default [
     { path: '/locatario/auth', layout: <PrivateRoute path='/locatario/auth' component={Auth} /> },
+    { path: '/locatario', layout: <PrivateRoute path='/locatario' component={HomeLocatario} /> },
 ]
