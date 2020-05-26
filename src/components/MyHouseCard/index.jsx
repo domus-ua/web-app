1909d807313ba612372584f6fe82a822c1312cbe
Francisco Aires <f.fontinha@ua.pt>
2020-05-26 16:44:03 +0100

    Added get of all my houses

---
+++ src/components/MyHouseCard/index.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { Redirect } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import CompareArrowsIcon from '@material-ui/icons/CompareArrows';
+
+import defaultImage from "assets/img/home/home.jpg";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: "100%",
+    marginTop: "30px",
+  },
+  media: {
+    height: 180,
+  },
+});
+
+function HouseCard({id, title, city, rooms, price, area, image}) {
+  const classes = useStyles();
+  
+  const [favorite, setFavorite] = useState(false);
+  const [compare, setCompare] = useState(false);
+  const [redirect, setRedirect] = useState(false);
+
+  if(image === undefined) {
+    image = defaultImage;
+  }
+
+  let favoriteButton = favorite === true ? "red" : "";
+  let compareButton = compare === true ? "#3f51b5" : "";
+
+  if(redirect) { return <Redirect to="house-details" />}
+  return (
+    <Card className={classes.root}>
+      <CardActionArea onClick={ () => {localStorage.setItem('currentHouse', JSON.stringify({id: id})); setRedirect(true);}}>
+        <CardMedia
+          className={classes.media}
+          image={image}
+          title={title}
+        />
+        <CardContent style={{minHeight: "200px"}}>
+          <Typography gutterBottom variant="h5" component="h5">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            <strong>City:</strong> {city}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            <strong>Rooms:</strong> {rooms}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            <strong>Price:</strong> {price} €
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            <strong>Habitable area:</strong> {area}m2
+          </Typography>
+
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button style={{backgroundColor: "#3f51b5", color: "white" }}>
+          Details
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+export default HouseCard;
--- src/layouts/Locador/MyHouses/index.jsx
+++ src/layouts/Locador/MyHouses/index.jsx
@@ -1,11 +1,126 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
+
+import Navbar from "components/Navbar2";
+import Footer from "components/Footer";
+import MyHouseCard from "components/MyHouseCard";
+import UserNavbar from "components/UserNavbar";
+
+import TextField from '@material-ui/core/TextField';
+
+import uris from "variables/uris";
+import {Button, Modal} from "react-bootstrap";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel/InputLabel";
+import Select from "@material-ui/core/Select/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 
 class MyHouses extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.myHouses = [];
+        this.state = {
+            houses: [],
+            searching: true
+        }
+
+        this.authUser = JSON.parse(localStorage.getItem('authUser'));
+        this.fetchMyHouses = this.fetchMyHouses.bind(this);
+        this.searching = this.searching.bind(this);
+
+
+    }
+
+    componentDidMount() {
+        this.fetchMyHouses();
+    }
+
+    fetchMyHouses() {
+
+        let locadorId = this.authUser.id;
+
+        fetch(uris.restApi.locadores + "/houses/" + locadorId, {
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            }
+        })
+            .then(response => {
+                if (!response.ok) throw new Error(response.status);
+                else return response.json();
+
+            })
+            .then(data => {
+                this.myHouses = data;
+                this.setState({
+                    searching: false,
+                    houses: this.myHouses
+                })
+            })
+            .catch(error => {
+                console.log("Fetch error: " + error);
+            })
+
+    }
+
+    searching() {
+        this.setState({ searching: true, foundHouses: [] })
+        setTimeout(() => {
+            this.fetchMyHouses();
+        }, 1500);
+    }
 
     render() {
         return (
             <div id="my-houses">
-                My Houses
+                {this.authUser === null ?
+                    <header className="with-banner">
+                        <Navbar />
+                    </header>
+
+                    : <header>
+                        <UserNavbar />
+                    </header>
+                }
+                <section className="alternate">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-sm-9">
+                                <h3 style={{ color: "#3f51b5" }}>My Houses</h3>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-sm-1">
+                                <hr style={{ marginTop: "-2px" }} />
+                            </div>
+                        </div>
+                        {
+                            this.state.searching &&
+                            <div className="row" style={{ marginTop: "30px" }}>
+                                <div className="col-sm-12 d-flex justify-content-center">
+                                    <div className="loader"></div>
+                                </div>
+                            </div>
+                        }
+                        <div className="row" style={{ marginTop: "15px" }}>
+                            {this.state.houses.map(house => {
+                                return <div className="col-sm-3">
+                                    <MyHouseCard
+                                        id={house.id}
+                                        title={house.name}
+                                        city={house.city}
+                                        rooms={house.noRooms}
+                                        price={house.price}
+                                        area={house.habitableArea}
+                                    />
+                                </div>
+                            })}
+                        </div>
+                    </div>
+                </section>
+                <Footer />
             </div>
         );
     }
